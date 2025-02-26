d67a2057fdbb53165ca5b27731dbc9d4c3301f06
Johan Isacsson <johan@generatorhallen.se>
2025-02-26 23:00:44 +0100

    ✨(oryx): Keyboard layout edited.

--- userspace/keyboards/zsa/voyager/keymaps/oryx.mbj5o/config.h
+++ userspace/keyboards/zsa/voyager/keymaps/oryx.mbj5o/config.h
@@ -3,8 +3,13 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+
+#define PERMISSIVE_HOLD
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "MBJ5o/wOODgd"
+#define SERIAL_NUMBER "MBJ5o/MaajWo"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
